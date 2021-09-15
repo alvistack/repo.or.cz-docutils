524e2a4094d2f07a030a7c6600ea6e8438139845
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-09-15 16:38:37 +0800

    docutils-0.17.1

---
+++ test/functional/tests/misc_rst_html4css1.py
@@ -0,0 +1,13 @@
+# Source and destination file names.
+test_source = "misc_rst_html4css1.txt"
+test_destination = "misc_rst_html4css1.html"
+
+# Keyword parameters passed to publish_file.
+reader_name = "standalone"
+parser_name = "rst"
+writer_name = "html4css1"
+
+# Settings
+# test for encoded attribute value in optional stylesheet name:
+settings_overrides['stylesheet'] = 'foo&bar.css, html4css1.css'
+settings_overrides['stylesheet_path'] = ''
